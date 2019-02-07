eae74a35c0e8c96a74634cb71af73aac641a2f57
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-02-07 10:18:38 -0500

    x's fixed

--- src/index.js
+++ src/index.js
@@ -29,9 +29,19 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (<
+      Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
